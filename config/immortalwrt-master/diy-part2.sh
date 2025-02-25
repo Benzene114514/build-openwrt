1c28ddb512f01486ef0d07e43a634aa8297821d5
Benzene114514 <103049146+Benzene114514@users.noreply.github.com>
2025-02-25 22:20:42 +0800

    Update diy-part2.sh

--- config/immortalwrt-master/diy-part2.sh
+++ config/immortalwrt-master/diy-part2.sh
@@ -30,3 +30,6 @@ svn co https://github.com/ophub/luci-app-amlogic/trunk/luci-app-amlogic package/
 #
 # ------------------------------- Other ends -------------------------------
 
+#####Add a feed source
+
+echo 'src-git amlogic https://github.com/ophub/luci-app-amlogic' >>feeds.conf.default
